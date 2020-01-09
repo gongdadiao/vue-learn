dba5a9a0f93976713e91362a16ef52c20a89d485
coder-gong <aaaaaabbbbbb12345@126.com>
2020-01-09 13:04:35 +0800

    vue第一次学习代码

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router"
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
+
+router.beforeEach((to,from,next)=>{
+  // eslint-disable-next-line no-undef,no-console
+  //console.log("前往的页面"+to.path);
+  // eslint-disable-next-line no-undef,no-console
+  //console.log("当前页面"+from.path);
+  // eslint-disable-next-line no-console
+  if(to.matched.some(record=>record.meta.requiredAuth)){
+    // eslint-disable-next-line no-console
+    console.log("这个页面需要权限");
+  }
+  next();
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h =>h(App)
 }).$mount('#app')
